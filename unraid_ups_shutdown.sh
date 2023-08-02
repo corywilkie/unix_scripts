a3d2e04fa23828c5e616fdf16a3a08856603e42e
corywilkie <52163034+corywilkie@users.noreply.github.com>
2023-08-02 14:45:05 -0300

    Create unraid_ups_shutdown.sh

---
+++ unraid_ups_shutdown.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+while true;
+do
+  i=0
+  
+  ping -c 1 10.0.1.1
+  if [ $? -eq 0 ]
+  then 
+    i=0 #Pass
+    echo '10.0.1.1 Pass i='$i
+  else
+    ((i++)) #Fail
+    echo '10.0.1.1 Fail i='$i
+  fi
+  sleep 60
+  
+  ping -c 1 10.0.1.5
+  if [ $? -eq 0 ]
+  then 
+    i=0 #Pass
+    echo '10.0.1.5 Pass i='$i
+  else
+    ((i++)) #Fail
+    echo '10.0.1.5 Fail i='$i
+  fi
+  sleep 60
+  
+  ping -c 1 8.8.8.8
+  if [ $? -eq 0 ]
+  then 
+    i=0 #Pass
+    echo '8.8.8.8 Pass i='$i
+  else
+    ((i++)) #Fail
+    echo '8.8.8.8 Fail i='$i
+  fi
+  sleep 60
+
+  if [ $i -gt 10 ]
+  then
+    /sbin/powerdown
+  fi
+    
+done
